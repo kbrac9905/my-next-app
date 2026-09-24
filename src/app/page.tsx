dd0689112fd4bd69a13f0082c04ac523f80e982f
kbrac9905 <kbrac9905@gmail.com>
2026-09-24 07:58:08 +0100

    update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Footer from '@/components/home/Footer';
 export default function Home() {
   return (
     <div className='min-h-screen flex flex-col'>
-    <h1>Deployed success 2 in cloudflare</h1>
+    
       <Navbar />
       <main className='max-w-6xl mx-auto flex-1 p-6'>
         <Header />
